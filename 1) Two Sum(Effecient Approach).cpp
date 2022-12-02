b6d519030cdea0c0d35dc82477c1de5329a9f86b
Ishaan Dixit <ishaandixit21@gmail.com>
2022-12-02 18:32:47 +0530

    1) Two Sum(Effecient Approach)
    Using Hashmap

---
+++ 1) Two Sum(Effecient Approach).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        vector<int> result;
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size() ; i++){
+            if(m.find(target - nums[i]) != m.end()){
+                
+                result.push_back(m[target - nums[i]]);
+                result.push_back(i);
+                break;
+            }
+            m.insert({nums[i], i});
+            
+        }
+        return result;
+        
+    }
+};

9728191cfe7dac3aa17a1cb5d71c3d2ac3d0b968
Ishaan Dixit <ishaandixit21@gmail.com>
2022-12-05 21:26:58 +0530

    Added new question

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool matching(char a,char b)
+    {
+    return (( a=='(' && b==')' )||( a=='[' && b==']' )||( a=='{' && b=='}' ));
+    }
+    bool isValid(string s) {
+        stack <char> st;
+        for(int i = 0; i<s.length();i++){
+            if((s[i] == '(') || (s[i] =='{') || (s[i] == '[')){
+                st.push(s[i]);
+            }
+            else{
+                if (st.empty()==true) 
+                    return false;
+            else if(matching(st.top(),s[i])==false)
+                    return false;
+            else
+                st.pop();
+                
+                    
+            }
+        }
+        if(st.empty() == true){
+            return true;
+            
+        }  
+        else{
+            return false;
+        }
+    }
+};

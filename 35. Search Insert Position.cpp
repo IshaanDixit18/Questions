7769293442b7f24dfd3ec514eaca329e054e61b4
Ishaan Dixit <ishaandixit21@gmail.com>
2022-12-09 18:38:08 +0530

    Added new question

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while(low <=high){
+            int mid = (high + low)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                high = mid - 1;
+            }
+            else if(target > nums[mid]){
+                low = mid + 1;
+            }
+            
+                
+        }
+        return low ;
+    }
+};
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 My Leetcode problem solving journey.
 Started in September 2022.
 
+
 Leetcode userid : ishaandixit18
